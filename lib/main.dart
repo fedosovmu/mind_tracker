d9628fc2f9f58e6b98ae9eb857e6e979ef9e7437
fedosovmu <fedosovmu@gmail.com>
2021-01-21 20:58:18 +0300

    Деплой на ios

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MindTrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
