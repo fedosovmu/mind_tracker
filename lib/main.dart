8d2b5727344c6df9d768ebd8c0171001b619424a
fedosovmu <fedosovmu@gmail.com>
2021-01-21 14:44:30 +0300

    Рефакторинг, страница онбординга вынесена в отдельный файл

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'on_boarding_page.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -14,60 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: OnBoardingPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: null,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: [
-              Container(
-                color: Colors.red,
-                width: double.infinity,
-                height: 400,
-              ),
-              Container(
-                color: Colors.purple,
-                height: 15,
-              ),
-              Container(
-                color: Colors.green,
-                alignment: Alignment.center,
-                padding: EdgeInsets.symmetric(vertical: 18, horizontal: 16),
-                child:  Text(
-                    "Следите за своим ментальным состоянием оценивая его по шкале с семью градациями",
-                    style: TextStyle(fontSize: 16)
-                ),
-              ),
-            ],
-          ),
-          Container(
-            color: Colors.yellow,
-            width: double.infinity,
-            padding: EdgeInsets.only(left: 16, right: 16, bottom: 8),
-            child: FlatButton (
-              color: Colors.blue,
-                height: 60,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(16)),
-                ),
-                onPressed: () { print("Next button pressed"); },
-                child: Text(
-                  "Далее",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                )
-            )
-          )
-        ],
-      )
-    );
-  }
-}
