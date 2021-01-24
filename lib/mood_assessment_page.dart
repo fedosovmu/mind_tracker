476271098eaaa0bd0594b8e9693af9fb9db6c118
fedosovmu <fedosovmu@gmail.com>
2021-01-25 02:00:28 +0300

    Продолжаю делать экран оценки настроения.  Добавлены новые ассеты

--- lib/mood_assessment_page.dart
+++ lib/mood_assessment_page.dart
@@ -9,17 +9,21 @@ class MoodAssessmentPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-            'Как ваше настроение?',
-          style: TextStyle(fontSize: dp(20)),
+          'Как ваше настроение?',
+          style: TextStyle(
+            fontSize: dp(20),
+
+          ),
         ),
-        leading:
-          IconButton(
-              icon: const Icon(Icons.close),
-              onPressed: () {}
-          )
-        ,
         backgroundColor: AppColors.moodAssessmentPage['backgroundColor'],
+        //backgroundColor: Colors.red,
         elevation: 0,
+        titleSpacing: 0,
+        toolbarHeight: dp(56),
+        leading: Container(
+          //color: Colors.yellow,
+        ),
+        leadingWidth: dp(56),
       ),
       body: Container(
         color: AppColors.moodAssessmentPage['backgroundColor'],
