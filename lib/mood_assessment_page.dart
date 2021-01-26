30da7aac30535a54f854410a0f0d9afdaa7468be
fedosovmu <fedosovmu@gmail.com>
2021-01-26 17:09:34 +0300

    Гтова заготовка для виджета выбора настроения

--- lib/mood_assessment_page.dart
+++ lib/mood_assessment_page.dart
@@ -30,56 +30,85 @@ class MoodAssessmentPage extends StatelessWidget {
       body: Container(
         color: AppColors.moodAssessmentPage['backgroundColor'],
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Container(
-              color: Colors.yellow,
-              height: dp(100),
-            ),
-            Container(
-              height: dp(136),
-              width: double.infinity,
-              padding: EdgeInsets.only(left: dp(16), right: dp(16), bottom: dp(8)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  FlatButton(
-                    onPressed: () {print('Assess Mood');},
-                    child: Text(
-                      'Оценить',
-                      style: TextStyle(
-                        color: AppColors.moodAssessmentPage['assessMoodButtonTextColor'],
-                        fontSize: dp(18),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    color: AppColors.moodAssessmentPage['assessMoodButtonColor'],
-                    height: dp(60),
-                    minWidth: double.infinity,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(dp(16)))
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: () {print('Skip');},
-                    child: Text(
-                      'Пропустить',
-                      style: TextStyle(
-                        color: AppColors.moodAssessmentPage['skipButtonTextColor'],
-                        fontSize: dp(18),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    height: dp(60),
-                    minWidth: double.infinity,
-                  ),
-                ],
-              ),
-            )
+            _buildMoodAssessor(),
+            _buildBottomButtons(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildMoodAssessor() {
+    return Expanded(
+      child: Center(
+        child: Container(
+          color: Colors.yellow,
+          height: dp(278),
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                color: Colors.blue,
+                height: dp(209),
+              ),
+              Positioned(
+                top: 0,
+                child: Container(
+                  color: Colors.red,
+                  width: dp(114),
+                  height: dp(114),
+                )
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+
+  Widget _buildBottomButtons() {
+    return           Container(
+      height: dp(136),
+      width: double.infinity,
+      padding: EdgeInsets.only(left: dp(16), right: dp(16), bottom: dp(8)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          FlatButton(
+            onPressed: () {print('Assess Mood');},
+            child: Text(
+              'Оценить',
+              style: TextStyle(
+                color: AppColors.moodAssessmentPage['assessMoodButtonTextColor'],
+                fontSize: dp(18),
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            color: AppColors.moodAssessmentPage['assessMoodButtonColor'],
+            height: dp(60),
+            minWidth: double.infinity,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(dp(16)))
+            ),
+          ),
+          FlatButton(
+            onPressed: () {print('Skip');},
+            child: Text(
+              'Пропустить',
+              style: TextStyle(
+                color: AppColors.moodAssessmentPage['skipButtonTextColor'],
+                fontSize: dp(18),
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            height: dp(60),
+            minWidth: dp(150),
+          ),
+        ],
       ),
     );
   }
