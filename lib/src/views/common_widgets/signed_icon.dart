6f1a56b6fc7c78607221211909b1ac9889dbf4c4
fedosovmu <fedosovmu@gmail.com>
2021-01-31 05:45:51 +0300

    Навигационный бар почти готов

--- lib/src/views/common_widgets/signed_icon.dart
+++ lib/src/views/common_widgets/signed_icon.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mind_tracker/src/views/utils/content.dart';
+import 'package:mind_tracker/src/views/utils/custom_colors.dart';
+import 'package:mind_tracker/src/views/utils/custom_text_styles.dart';
 import '../utils/metrics.dart';
 
 
@@ -7,15 +9,26 @@ class SignedIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      padding: EdgeInsets.only(top: dp(8)),
       child: IconButton(
         padding: EdgeInsets.zero,
         splashRadius: dp(44),
-        iconSize: dp(56),
-        icon: Container(
-            //width: dp(57),
-            //height: dp(44),
-            color: Colors.red
+        iconSize: dp(60),
+        icon: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icons/home.png',
+              color: CustomColors.purpleTextSecondary,
+              width: dp(24),
+              height: dp(24),
+            ),
+            Text(
+              'Главный',
+              style: CustomTextStyles.basic.copyWith(
+                  color: CustomColors.silverWhite),
+            ),
+          ],
         ),
         onPressed: () { print('Bottom menu pressed'); },
       ),
