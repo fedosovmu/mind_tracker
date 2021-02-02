172c38d94fb9de3dae07082f3a168dea66542f12
fedosovmu <fedosovmu@gmail.com>
2021-02-03 00:40:29 +0300

    Удалил явное объявление анимации переключения экранов потому, что по умолчанию она и так кастомная

--- lib/src/views/utils/app_theme.dart
+++ lib/src/views/utils/app_theme.dart
@@ -4,12 +4,6 @@ import 'custom_colors.dart';
 
 
 final appTheme = ThemeData(
-  pageTransitionsTheme: PageTransitionsTheme(
-    builders: {
-      TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-      TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
-    }
-  ),
   brightness: Brightness.dark,
   scaffoldBackgroundColor: CustomColors.purpleDark,
   accentColor: CustomColors.main,
