0fc09de406a05c63e8789f67499f90e562f7118f
fedosovmu <fedosovmu@gmail.com>
2021-02-12 19:44:33 +0300

    Переделываю экран аналитики. Добавлены вкладки

--- lib/src/views/screens/main_screen/analytics_page/analytics_page.dart
+++ lib/src/views/screens/main_screen/analytics_page/analytics_page.dart
@@ -1,56 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:mind_tracker/src/views/common_widgets/custom_app_bar.dart';
-import 'package:mind_tracker/src/views/screens/main_screen/analytics_page/mood_chart/mood_chart.dart';
+import 'package:mind_tracker/src/views/utils/theme/custom_colors.dart';
 import 'package:mind_tracker/src/views/utils/theme/custom_text_styles.dart';
 import 'package:provider/provider.dart';
 import '../../../utils/metrics.dart';
 
 
-class AnalyticsPage extends StatelessWidget {
+class AnalyticsPage extends StatefulWidget {
+
+  @override
+  _AnalyticsPageState createState() => _AnalyticsPageState();
+}
+
+class _AnalyticsPageState extends State<AnalyticsPage> with TickerProviderStateMixin {
+  var _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 2,
+      vsync: this
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
-        title: 'График настроения',
+        title: 'Аналитика',
       ),
       body: Column(
         children: [
-          _buildTabs(),
+          _buildTabBar(),
           Expanded(
-            child: SingleChildScrollView(
-              child: Column(
+            child: TabBarView(
+              controller: _tabController,
                 children: [
-                  MoodChart(),
-                  _buildCards()
-                ],
-              )
+                  Container(
+                    color: Colors.green,
+                  ),
+                  Container(
+                    color: Colors.red,
+                  )
+                ]
             ),
           )
         ],
-      )
-    );
-  }
-
-  Widget _buildTabs() {
-    return Container(
-      color: Colors.green,
-      width: double.infinity,
-      height: dp(40),
-      child: Center(
-        child: Text(
-          'График                                Влияние',
-          style: CustomTextStyles.basicH1Medium,
-        ),
       ),
     );
   }
 
-  Widget _buildCards() {
-    return Container(
-      margin: EdgeInsets.only(top: dp(18)),
-      color: Colors.yellow,
-      width: dp(327),
-      height: dp(500),
+  Widget _buildTabBar () {
+    return Stack(
+      alignment: Alignment.bottomLeft,
+      //width: double.infinity,
+      children: [
+        Container(
+          color: CustomColors.purpleSuperDark,
+          width: double.infinity,
+          height: dp(2),
+        ),
+        TabBar(
+          isScrollable: false,
+          labelStyle: CustomTextStyles.basicH1Medium,
+          labelColor: CustomColors.purpleLight,
+          unselectedLabelStyle: CustomTextStyles.basicH1Medium,
+          unselectedLabelColor: CustomColors.purpleTextSecondary,
+          indicatorColor: CustomColors.purpleLight,
+          tabs: [
+            Tab(text: 'График',),
+            Tab(text: 'Влияние',)
+          ],
+          controller: _tabController
+        ),
+      ],
     );
   }
 }
---
+++ lib/src/views/screens/main_screen/analytics_page/mood_chart_page/mood_chart_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mind_tracker/src/views/screens/main_screen/analytics_page/mood_chart_page/mood_chart/mood_chart.dart';
+
+
+class MoodChartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      width: double.infinity,
+      height: double.infinity,
+    );
+  }
+
+}
