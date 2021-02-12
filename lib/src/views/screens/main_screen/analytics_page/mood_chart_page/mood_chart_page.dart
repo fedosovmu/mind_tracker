1c5f21ae87ad424d3f508ca4f6c082a3078f8ee3
fedosovmu <fedosovmu@gmail.com>
2021-02-12 20:20:29 +0300

    Начал делать экран с графиком

--- lib/src/views/screens/main_screen/analytics_page/mood_chart_page/mood_chart_page.dart
+++ lib/src/views/screens/main_screen/analytics_page/mood_chart_page/mood_chart_page.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:mind_tracker/src/views/utils/metrics.dart';
 import 'package:mind_tracker/src/views/screens/main_screen/analytics_page/mood_chart_page/mood_chart/mood_chart.dart';
 
 
 class MoodChartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
-      width: double.infinity,
-      height: double.infinity,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          MoodChart(),
+          Container(
+            margin: EdgeInsets.only(top: dp(11), left: dp(16), right: dp(16)),
+            color: Colors.yellow,
+            width: double.infinity,
+            height: dp(800),
+          )
+        ],
+      ),
     );
   }
 
