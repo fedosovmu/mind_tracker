5014b2827cce30169fe0a8e2173eb039a1d35b4a
fedosovmu <fedosovmu@gmail.com>
2021-02-12 23:54:50 +0300

    Готовы кнопки выбора периода

--- lib/src/views/screens/main_screen/analytics_page/mood_chart_page/mood_chart_card/mood_chart_card.dart
+++ lib/src/views/screens/main_screen/analytics_page/mood_chart_page/mood_chart_card/mood_chart_card.dart
@@ -17,7 +17,7 @@ class MoodChartCard extends StatelessWidget {
       ),
       child: Column(
         children: [
-          _buildTabs(),
+          PeriodToggleButtons(),
           Container(
             padding: EdgeInsets.only(top: dp(14), left: dp(16)),
             //color: Colors.blue,
@@ -31,7 +31,7 @@ class MoodChartCard extends StatelessWidget {
           _buildMoodChart(),
           Expanded(
             child: Container(
-              //color: Colors.green,
+              color: Colors.green,
             ),
           )
         ],
@@ -41,22 +41,90 @@ class MoodChartCard extends StatelessWidget {
 
   Widget _buildMoodChart() {
     return Container(
+      //margin: EdgeInsets.only(left: dp(0), top: dp(0), right: dp(0)),
       color: Colors.orange,
-      width: double.infinity,
+      //width: double.infinity,
       height: dp(220),
     );
   }
+}
 
-  Widget _buildTabs() {
+
+class PeriodToggleButtons extends StatefulWidget {
+  final Function onPeriodChange;
+
+  PeriodToggleButtons({this.onPeriodChange});
+
+  @override
+  _PeriodToggleButtonsState createState() => _PeriodToggleButtonsState();
+}
+
+class _PeriodToggleButtonsState extends State<PeriodToggleButtons> {
+  int _selectedIndex = 0;
+  static const _periodNames = ['Неделя', 'Месяц', 'Период'];
+  var _onPressCallBacks;
+
+  @override
+  void initState() {
+    super.initState();
+    _onPressCallBacks = List.generate(_periodNames.length, (index) {
+      return () {
+        setState(() {
+          _selectedIndex = index;
+        });
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: dp(16), left: dp(12)),
-      //color: Colors.red,
-      width: dp(303),
-      height: dp(26+16),
-      child: Text(
-        'Неделя       Месяц       Период',
-        style: CustomTextStyles.basic.copyWith(color: CustomColors.purpleLight),
+      margin: EdgeInsets.only(left: dp(12), right: dp(12), top: dp(16)),
+      height: dp(26),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: List.generate(_periodNames.length, (index) {
+          return PeriodButton(
+            _periodNames[index],
+            isPressed: index == _selectedIndex,
+            onPressed: () {
+              setState(() {
+                _selectedIndex = index;
+                print('Perion button $index was pressed');
+              });
+            }
+          );
+        }),
       ),
     );
   }
+}
+
+class PeriodButton extends StatelessWidget {
+  final String title;
+  final Function onPressed;
+  final bool isPressed;
+  
+  PeriodButton(this.title, {this.onPressed, this.isPressed = false});
+  
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      splashColor: Color(0x00),
+      highlightColor: Color(0x00),
+      color: isPressed ? CustomColors.purpleMegaDark : CustomColors.purpleDark,
+      minWidth: dp(93),
+      child: Text(
+        title,
+        style: CustomTextStyles.basic.copyWith(
+          color: CustomColors.purpleLight
+        ),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(dp(8)))
+      ),
+      onPressed: onPressed
+    );
+  }
+
 }
