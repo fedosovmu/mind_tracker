1d184322a6cae2af818055cde473b3d719de06c6
fedosovmu <fedosovmu@gmail.com>
2021-02-17 13:56:39 +0300

    Добавлена анонимная авторизация

--- lib/src/business_logic/services/firebase_provider.dart
+++ lib/src/business_logic/services/firebase_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mind_tracker/src/business_logic/models/mood_assessment.dart';
@@ -6,14 +7,17 @@ import 'package:mind_tracker/src/business_logic/models/mood_assessment.dart';
 
 abstract class FirebaseProvider {
   static final _mood_assessments_collection = FirebaseFirestore.instance.collection('mood_assessments');
+  static UserCredential _userCredential;
 
   static void initializeFirebaseConnection () async {
     final firebaseInitializeData = await Firebase.initializeApp();
+    _userCredential = await FirebaseAuth.instance.signInAnonymously();
+    print('=== USER: ${_userCredential.user.uid}');
   }
 
   static Future<List<MoodAssessment>> getMoodAssessments () async {
-    final moodAssessmentsQuerySnapshot = await _mood_assessments_collection.get();
-        //.where('user_id', isEqualTo: 'DocumentReference(users/test_user)')
+    final moodAssessmentsQuerySnapshot = await _mood_assessments_collection
+        .where('user_id', isEqualTo: _userCredential.user.uid).get();
         //.where('date', isEqualTo: '2021-02-09').get();
 
     final List<MoodAssessment> moodAssessments = moodAssessmentsQuerySnapshot.docs.map((mood_assessment_doc) {
@@ -24,8 +28,10 @@ abstract class FirebaseProvider {
   }
 
   static void addMoodAssessment(MoodAssessment moodAssessment) {
+    var moodAssessmentMap = moodAssessment.toMap();
+    moodAssessmentMap['user_id'] = _userCredential.user.uid;
     _mood_assessments_collection.add(
-        moodAssessment.toMap()
+        moodAssessmentMap
     ).then((value) => print('=== FIREBASE ADD ($moodAssessment)'));
   }
 }
