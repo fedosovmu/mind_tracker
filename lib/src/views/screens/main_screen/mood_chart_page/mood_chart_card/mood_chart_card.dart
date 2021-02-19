b6ea770b028f88d2b4072153bf63d42ac6b9e869
fedosovmu <fedosovmu@gmail.com>
2021-02-19 19:03:24 +0300

    Экспериментирую с размером графика

--- lib/src/views/screens/main_screen/mood_chart_page/mood_chart_card/mood_chart_card.dart
+++ lib/src/views/screens/main_screen/mood_chart_page/mood_chart_card/mood_chart_card.dart
@@ -3,7 +3,6 @@ import 'package:mind_tracker/src/views/screens/main_screen/mood_chart_page/'
     'mood_chart_card/mood_chart.dart';
 import 'package:mind_tracker/src/views/screens/main_screen/mood_chart_page/'
     'mood_chart_card/mood_chart_date_labels.dart';
-import 'package:mind_tracker/src/views/common_widgets/period_buttons/period_toggle_buttons.dart';
 import 'package:mind_tracker/src/views/utils/metrics.dart';
 import 'package:mind_tracker/src/views/utils/theme/custom_colors.dart';
 import 'package:mind_tracker/src/views/utils/theme/custom_text_styles.dart';
@@ -13,7 +12,7 @@ class MoodChartCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(left: dp(16), right: dp(16), top: dp(10), bottom: dp(30)),
+      margin: EdgeInsets.only(left: dp(16), right: dp(16), top: dp(10), bottom: dp(250)),
       padding: EdgeInsets.only(top: dp(5), bottom: dp(13)),
       decoration: BoxDecoration(
         color: CustomColors.purpleSuperDark,
