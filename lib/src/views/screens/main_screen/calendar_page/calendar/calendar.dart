125a023d3e42f10d80b381d11afb9eaffe5d82fe
fedosovmu <fedosovmu@gmail.com>
2021-03-16 15:18:36 +0300

     Промежуточный комит. Реализую календарь

--- lib/src/views/screens/main_screen/calendar_page/calendar/calendar.dart
+++ lib/src/views/screens/main_screen/calendar_page/calendar/calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mind_tracker/src/views/utils/content.dart';
 import 'package:mind_tracker/src/views/utils/metrics.dart';
 import 'package:mind_tracker/src/views/utils/theme/custom_colors.dart';
 import 'package:mind_tracker/src/views/utils/theme/custom_text_styles.dart';
@@ -7,28 +8,44 @@ import 'widgets/calendar_day_of_week_labels.dart';
 import 'calendar_day_buttons/calendar_day_buttons.dart';
 
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   PageController _pageController;
-
-  static const _pageChangeAnimationDuration = Duration(milliseconds: 500);
-  static const _pageChangeAnimationCurve = Curves.ease;
 
   Calendar () {
     final now = DateTime.now();
     _pageController = PageController();
   }
 
-  Widget _buildCalendarPage(int pageIndex) {
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  static const _pageChangeAnimationDuration = Duration(milliseconds: 500);
+  static const _pageChangeAnimationCurve = Curves.ease;
+
+  var _selectedYear;
+  var _selectedMonth;
+
+  @override
+  void initState() {
     final now = DateTime.now();
-    var newMonthDate = DateTime(now.year, now.month - pageIndex);
-    return CalendarDayButtons(
-      year: newMonthDate.year,
-      month: newMonthDate.month,
-    );
+    _selectedYear = now.year;
+    _selectedMonth = now.month;
+  }
+
+  DateTime _getDateOfCalendarPage(int pageIndex) {
+    final now = DateTime.now();
+    return DateTime(now.year, now.month - pageIndex);
   }
 
   void _onSlideChanged(int page) {
     print('On page slide changed to $page');
+    setState(() {
+      final newDate = _getDateOfCalendarPage(page);
+      _selectedYear = newDate.year;
+      _selectedMonth = newDate.month;
+    });
   }
 
   @override
@@ -42,25 +59,42 @@ class Calendar extends StatelessWidget {
       ),
       child: Column(
         children: [
-          MonthSwitchButtons(
-            onLeftArrowPressed: () {
-              print('Left arrow pressed');
-              _pageController.nextPage(duration: _pageChangeAnimationDuration, curve: _pageChangeAnimationCurve);
-            },
-            onRightArrowPressed: () {
-              print('on right arrow pressed');
-              _pageController.previousPage(duration: _pageChangeAnimationDuration, curve: _pageChangeAnimationCurve);
-            },
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: dp(16)),
+            height: dp(60),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '$_selectedYear ${Content.monthNames[_selectedMonth]}',
+                  style: CustomTextStyles.basicH1Medium,
+                ),
+                MonthSwitchButtons(
+                  onLeftArrowPressed: () {
+                    print('Left arrow pressed');
+                    widget._pageController.nextPage(duration: _pageChangeAnimationDuration, curve: _pageChangeAnimationCurve);
+                  },
+                  onRightArrowPressed: () {
+                    print('on right arrow pressed');
+                    widget._pageController.previousPage(duration: _pageChangeAnimationDuration, curve: _pageChangeAnimationCurve);
+                  },
+                ),
+              ],
+            ),
           ),
           CalendarDayOfWeekLabels(),
           Expanded(
             child: PageView.builder(
               onPageChanged: _onSlideChanged,
               reverse: true,
-              controller: _pageController,
+              controller: widget._pageController,
               itemBuilder: (BuildContext context, int index) {
                 print('Calendar page $index');
-                return _buildCalendarPage(index);
+                var newMonthDate = _getDateOfCalendarPage(index);
+                return CalendarDayButtons(
+                  year: newMonthDate.year,
+                  month: newMonthDate.month,
+                );
               }
             )
           ),
--- lib/src/views/screens/main_screen/calendar_page/calendar/month_switch_buttons/month_switch_buttons.dart
+++ lib/src/views/screens/main_screen/calendar_page/calendar/month_switch_buttons/month_switch_buttons.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mind_tracker/src/views/utils/content.dart';
 import 'package:mind_tracker/src/views/utils/metrics.dart';
-import 'package:mind_tracker/src/views/utils/theme/custom_text_styles.dart';
 import 'month_switch_button.dart';
 
 
@@ -13,26 +11,13 @@ class MonthSwitchButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: dp(16)),
-      height: dp(60),
+    return SizedBox(
+      width: dp(32 + 31 + 24),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            '2021 Март',
-            style: CustomTextStyles.basicH1Medium,
-          ),
-          SizedBox(
-            width: dp(32 + 31 + 24),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                MonthSwitchButton(onPressed: onLeftArrowPressed),
-                MonthSwitchButton(onPressed: onRightArrowPressed, isRight: true)
-              ],
-            ),
-          )
+          MonthSwitchButton(onPressed: onLeftArrowPressed),
+          MonthSwitchButton(onPressed: onRightArrowPressed, isRight: true)
         ],
       ),
     );
