2c061cab64f7b36cb7fef888a6b26902412604ea
fedosovmu <fedosovmu@gmail.com>
2021-04-03 21:51:43 +0300

    Рефакторинг. Изменяю архитектуру приложения

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'src/business_logic/viewmodels/providers_initializer.dart';
-import 'dart:async';
 import 'src/app.dart';
 
 
@@ -9,8 +8,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final moodAssessmentsProvider = await ProvidersInitializer.getMoodAssessmentsProvider();
   runApp(
-      ChangeNotifierProvider(
-        create: (context) => moodAssessmentsProvider,
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) {
+            return moodAssessmentsProvider;
+          })
+        ],
         child: MindTrackerApp(),
       )
   );
