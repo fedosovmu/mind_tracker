27dcf063b5118c08962044d29fb3889b304c7858
fedosovmu <fedosovmu@gmail.com>
2021-04-20 15:50:47 +0300

    Рефакторинг, виджет с панелью выхода из аккаунта вынесен в отдельный файл

---
+++ lib/src/views/screens/other/settings_screen/widgets/login_out_panel.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:mind_tracker/src/business_logic/viewmodels/auth_provider.dart';
+import 'package:mind_tracker/src/views/utils/metrics.dart';
+import 'package:mind_tracker/src/views/utils/theme/custom_colors.dart';
+import 'package:mind_tracker/src/views/utils/theme/custom_text_styles.dart';
+import 'package:provider/provider.dart';
+
+
+class LoginOutPanel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AuthProvider>(
+        builder: (context, authProvider, child) {
+          return Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Выход из аккаунта',
+                  style: CustomTextStyles.titleH1.copyWith(
+                      color: CustomColors.purpleMedium
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                        'User id: ${authProvider.uid}',
+                        style: CustomTextStyles.caption.copyWith(
+                            color: CustomColors.purpleLight
+                        )
+                    ),
+                    TextButton(
+                        child: Text(
+                          'Выйти',
+                          style: CustomTextStyles.basic.copyWith(
+                              color: Colors.blue
+                          ),
+                        ),
+                        onPressed: () {
+                          print('Exit button pressed');
+                          if (authProvider.isAuthorized) {
+                            authProvider.signOut();
+                            Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false);
+                          }
+                        }
+                    )
+                  ],
+                ),
+                SizedBox(height: dp(32))
+              ],
+            ),
+          );
+        }
+    );
+  }
+}
