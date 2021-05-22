e30a90f85d114533cd8fa0137c6b660d6f465a8e
fedosovmu <fedosovmu@gmail.com>
2021-05-23 00:33:21 +0300

    Промежуточный комит. Изменяю форму углов

---
+++ lib/src/views/utils/theme/custom_border_shape.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomBorderShape extends ContinuousRectangleBorder {
+  static const _correctionConstant = 2.4; // Умножение на даннную константу придает углу нужную форму
+
+  CustomBorderShape (double radius) : super (
+    borderRadius: BorderRadius.all(Radius.circular(radius * _correctionConstant))
+  );
+}
