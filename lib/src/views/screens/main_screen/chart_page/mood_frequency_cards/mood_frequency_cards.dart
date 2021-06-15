0c4861fa3ff54c724c327b50c656ca041085366b
fedosovmu <fedosovmu@gmail.com>
2021-06-15 16:08:41 +0300

    Теперь карточки под графиком отображаются корректно даже если оценки за определенное время суток отсутствуют

--- lib/src/views/screens/main_screen/chart_page/mood_frequency_cards/mood_frequency_cards.dart
+++ lib/src/views/screens/main_screen/chart_page/mood_frequency_cards/mood_frequency_cards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mind_tracker/src/business_logic/models/mood_assessment.dart';
 import 'package:mind_tracker/src/business_logic/models/part_of_day.dart';
 import 'package:mind_tracker/src/business_logic/viewmodels/mood_assessments_provider.dart';
 import 'package:mind_tracker/src/views/screens/main_screen/chart_page/mood_frequency_cards/'
@@ -14,51 +15,46 @@ class MoodFrequencyCards extends StatelessWidget {
 
   MoodFrequencyCards({@required this.startDate, @required this.endDate});
 
-  Widget _createMoodFrequencyCard(PartOfDay partOfDay) {
-    return Consumer<MoodAssessmentsProvider>(builder: (context, moodAssessmentsProvider, child) {
-      final moodAssessmentsForPeriod = moodAssessmentsProvider.getMoodAssessmentsForPeriod(
-          startDate: startDate,
-          endDate: endDate
-      ).toList();
-      final moodAssessmentForPartOfDay = moodAssessmentsForPeriod.where(
-              (moodAssessment) => moodAssessment.partOfDay == partOfDay).toList();
-      if (moodAssessmentForPartOfDay.isEmpty) {
-        return null;
-      }
-      final moodAssessmentsGroupedByMood = moodAssessmentForPartOfDay.groupListsBy(
-              (moodAssessment) => moodAssessment.mood);
+  Widget _createMoodFrequencyCard(PartOfDay partOfDay, List<MoodAssessment> moodAssessmentsForPeriod) {
+    final moodAssessmentForPartOfDay = moodAssessmentsForPeriod.where(
+            (moodAssessment) => moodAssessment.partOfDay == partOfDay).toList();
+    if (moodAssessmentForPartOfDay.length == 0) {
+      return null;
+    }
+    final moodAssessmentsGroupedByMood = moodAssessmentForPartOfDay.groupListsBy(
+            (moodAssessment) => moodAssessment.mood);
 
-      int maxMood = 1;
-      int maxMoodsCount = 0;
-      for (int mood = 1; mood <= 7; mood++) {
-        final hasMood = moodAssessmentsGroupedByMood.containsKey(mood);
-        if (hasMood) {
-          final moodsCount = moodAssessmentsGroupedByMood[mood].length;
-          if (moodsCount >= maxMoodsCount) {
-            maxMood = mood;
-            maxMoodsCount = moodsCount;
-          }
+    int maxMood = 1;
+    int maxMoodsCount = 0;
+    for (int mood = 1; mood <= 7; mood++) {
+      final hasMood = moodAssessmentsGroupedByMood.containsKey(mood);
+      if (hasMood) {
+        final moodsCount = moodAssessmentsGroupedByMood[mood].length;
+        if (moodsCount >= maxMoodsCount) {
+          maxMood = mood;
+          maxMoodsCount = moodsCount;
         }
       }
-      final moodAssessmentsWithSameMoodCount = moodAssessmentForPartOfDay.where(
-              (moodAssessment) => moodAssessment.mood == maxMood).length;
-      final moodAssessmentsAllCount = moodAssessmentForPartOfDay.length;
+    }
+    final moodAssessmentsWithSameMoodCount = moodAssessmentForPartOfDay.where(
+            (moodAssessment) => moodAssessment.mood == maxMood).length;
+    final moodAssessmentsAllCount = moodAssessmentForPartOfDay.length;
 
-      return MoodFrequencyCard(
-        mood: maxMood,
-        partOfDay: partOfDay,
-        moodAssessmentsWithSameMoodCount: moodAssessmentsWithSameMoodCount,
-        moodAssessmentsAllCount: moodAssessmentsAllCount,
-      );
-    });
+    return MoodFrequencyCard(
+      mood: maxMood,
+      partOfDay: partOfDay,
+      moodAssessmentsWithSameMoodCount: moodAssessmentsWithSameMoodCount,
+      moodAssessmentsAllCount: moodAssessmentsAllCount,
+    );
   }
 
-  List<Widget> _getMoodFrequencyCards() {
+  List<Widget> _getMoodFrequencyCards(List<MoodAssessment> moodAssessmentsForPeriod) {
     final List<Widget> moodFrequencyCards = [];
     final partOfDayCount = PartOfDay.values.length;
     for (int i = 0; i < partOfDayCount; i++) {
       final partOfDay = PartOfDay.values[(i + 1) % partOfDayCount];
-      final card = _createMoodFrequencyCard(partOfDay);
+      final card = _createMoodFrequencyCard(partOfDay, moodAssessmentsForPeriod);
+      print('=== Card: $partOfDay : $card');
       if (card != null) {
         moodFrequencyCards.add(card);
       }
@@ -71,14 +67,20 @@ class MoodFrequencyCards extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: dp(16), vertical: dp(8)),
       height: MoodFrequencyCard.height * 2 + dp(8*3),
-      child: GridView.count(
-        physics: NeverScrollableScrollPhysics(),
-        crossAxisSpacing: dp(8),
-        mainAxisSpacing: dp(8),
-        crossAxisCount: 2,
-        childAspectRatio: MoodFrequencyCard.width / MoodFrequencyCard.height,
-        children: _getMoodFrequencyCards(),
-      ),
+      child: Consumer<MoodAssessmentsProvider>(
+        builder: (context, moodAssessmentProvider, child) {
+          final moodAssessmentsForPeriod = moodAssessmentProvider
+              .getMoodAssessmentsForPeriod(startDate: startDate, endDate: endDate);
+          return GridView.count(
+            physics: NeverScrollableScrollPhysics(),
+            crossAxisSpacing: dp(8),
+            mainAxisSpacing: dp(8),
+            crossAxisCount: 2,
+            childAspectRatio: MoodFrequencyCard.width / MoodFrequencyCard.height,
+            children: _getMoodFrequencyCards(moodAssessmentsForPeriod),
+          );
+        },
+      )
     );
   }
 }
